42cb4f50068a82d21929bd446820e1c0bb3c20d0
Juan Garcia <19002409@alumnos.utleon.edu.mx>
2024-09-24 20:48:44 -0600

    Cuarto commit 24/08/24 distancias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
-import { ProductoFilterPipe } from './product/producto-filter.pipe'; // Importa el componente
+import { ProductoFilterPipe } from './product/producto-filter.pipe';
+import { Ejemplo1Component } from './formularios/ejemplo1/ejemplo1.component';
+import { DistanciaComponent } from './formularios/distancia/distancia.component'; // Importa el componente
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductoFilterPipe  // Declara el componente aquí
+    ProductoFilterPipe,
+    Ejemplo1Component,
+    DistanciaComponent  // Declara el componente aquí
   ],
   imports: [
     BrowserModule,
-    FormsModule  // Importa FormsModule para usar [(ngModel)]
+    FormsModule,  // Importa FormsModule para usar [(ngModel)] si es necesario
+    ReactiveFormsModule  // Si estás usando formularios reactivos
   ],
   providers: [],
   bootstrap: [AppComponent]
